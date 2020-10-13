03af50b23d9f7a74d791c8fe93c9c15f3226b9e3
Yair <al046638@uacam.mx>
2020-10-13 16:08:54 -0500

    Import de librerías SQL
    Se importo las librerías para crear la conexión a la base de datos.

---
+++ src/TiendaApp.java
@@ -0,0 +1,16 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author Final
+ */
+public class TiendaApp {
+    
+    public static void main(String[] args) {
+        
+    }
+}
