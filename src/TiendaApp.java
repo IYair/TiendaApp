227bff7f6d0537140037dc99dd186d50d0c5b1a5
Yair <al046638@uacam.mx>
2020-10-14 00:33:50 -0500

    Conexión a la base de datos
    Se agrego la conexión a la base de datos con la validación.

--- src/TiendaApp.java
+++ src/TiendaApp.java
@@ -10,7 +10,12 @@ import java.sql.Statement;
  */
 public class TiendaApp {
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException{
         
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        String UrlConexion ="jdbc:sqlserver://ENYAOFICIAL\\MMSQLSERVER:1433;databaseName=TiendaOnline;user=sa;password=12345;";
+        Connection con= DriverManager.getConnection(UrlConexion);
+        
+        System.out.println("Conexion establecida");
     }
 }
