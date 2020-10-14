5140bc14dbc78851b3c2ec3e465da27819b0bd93
Yair <al046638@uacam.mx>
2020-10-14 03:27:40 -0500

    Class Conexión
    Conexión a la base de datos "TiendaOnline"

---
+++ src/Modelo/Conexion.java
@@ -0,0 +1,32 @@
+package Modelo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+/**
+ *
+ * @author Final
+ */
+public class Conexion {
+    
+    String UrlConexion ="jdbc:sqlserver://ENYAOFICIAL\\MMSQLSERVER:1433;databaseName=TiendaOnline;user=sa;password=12345;";
+    private Connection conexion=null;
+    public Connection getConexion(){
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            conexion=(Connection) DriverManager.getConnection(UrlConexion);
+
+        } catch (SQLException ex) {
+            
+        }
+        catch(ClassNotFoundException ex){
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return conexion;
+    }
+}
