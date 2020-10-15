c8d7b3bc4eeda26e9e300e085bbe07399aab6328
Yair <al046638@uacam.mx>
2020-10-15 18:43:40 -0500

    Getters y Setter Usuario
    Se crearon los get y set de la clase usuario para su variables en la base de datos.

--- src/Modelo/Usuario.java
+++ src/Modelo/Usuario.java
@@ -10,5 +10,68 @@ package Modelo;
  * @author Final
  */
 public class Usuario {
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String Password) {
+        this.Password = Password;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getApellido() {
+        return Apellido;
+    }
+
+    public void setApellido(String Apellido) {
+        this.Apellido = Apellido;
+    }
+
+    public String getCorreo() {
+        return Correo;
+    }
+
+    public void setCorreo(String Correo) {
+        this.Correo = Correo;
+    }
+
+    public String getDateborn() {
+        return Dateborn;
+    }
+
+    public void setDateborn(String Dateborn) {
+        this.Dateborn = Dateborn;
+    }
+
+    public Byte getEstado() {
+        return Estado;
+    }
+
+    public void setEstado(Byte Estado) {
+        this.Estado = Estado;
+    }
+    private int id;
+    private String Password;
+    private String Nombre;
+    private String Apellido;
+    private String Correo;
+    private String Dateborn;
+    private Byte Estado;
     
 }
