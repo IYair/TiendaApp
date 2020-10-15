f487c79b48035509e6f815e3c2cb84abbac37803
Yair <al046638@uacam.mx>
2020-10-14 22:37:14 -0500

    Reset Main

--- src/TiendaApp.java
+++ src/TiendaApp.java
@@ -1,9 +1,3 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-
 /**
  *
  * @author Final
@@ -12,10 +6,5 @@ public class TiendaApp {
     
     public static void main(String[] args) {
         
-<<<<<<< Updated upstream
-=======
-
-        
->>>>>>> Stashed changes
     }
 }
