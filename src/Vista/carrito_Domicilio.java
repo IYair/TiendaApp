d7d9bca5e04d46238400018d2696a5b9e59981eb
Eduardo <al047554@uacam.mx>
2020-10-16 14:59:17 -0500

    Frame carrito_Domicilio
    Se crea el formulario carrito_Domicilio, encargado de recibir los datos despues de seleccionar un metodo de pago.

--- src/Vista/carrito_Domicilio.java
+++ src/Vista/carrito_Domicilio.java
@@ -27,21 +27,94 @@ public class carrito_Domicilio extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel_calle = new javax.swing.JLabel();
+        jTextField_calle = new javax.swing.JTextField();
+        jLabel_noExt = new javax.swing.JLabel();
+        jTextField_NoExt = new javax.swing.JTextField();
+        jLabel_CP = new javax.swing.JLabel();
+        jTextField_CP = new javax.swing.JTextField();
+        jLabel_estado = new javax.swing.JLabel();
+        jTextField_estado = new javax.swing.JTextField();
+        jLabel_Ciudad = new javax.swing.JLabel();
+        jTextField_ciudad = new javax.swing.JTextField();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setBackground(new java.awt.Color(253, 231, 76));
+
+        jLabel_calle.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        jLabel_calle.setText("Calle");
+
+        jLabel_noExt.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        jLabel_noExt.setText("No. exterior");
+
+        jLabel_CP.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        jLabel_CP.setText("CP");
+
+        jTextField_CP.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextField_CPActionPerformed(evt);
+            }
+        });
+
+        jLabel_estado.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        jLabel_estado.setText("Estado");
+
+        jLabel_Ciudad.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        jLabel_Ciudad.setText("Ciudad");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(31, 31, 31)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel_Ciudad, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel_estado, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel_calle)
+                    .addComponent(jTextField_calle, javax.swing.GroupLayout.PREFERRED_SIZE, 258, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                        .addComponent(jTextField_CP, javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(jTextField_NoExt, javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(jLabel_noExt, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jLabel_CP, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                        .addComponent(jTextField_estado, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 160, Short.MAX_VALUE)
+                        .addComponent(jTextField_ciudad, javax.swing.GroupLayout.Alignment.LEADING)))
+                .addContainerGap(111, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(26, 26, 26)
+                .addComponent(jLabel_calle)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jTextField_calle, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jLabel_noExt)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jTextField_NoExt, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jLabel_CP)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jTextField_CP, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jLabel_Ciudad)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jTextField_ciudad, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jLabel_estado)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jTextField_estado, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(60, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jTextField_CPActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField_CPActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTextField_CPActionPerformed
 
     /**
      * @param args the command line arguments
@@ -79,5 +152,15 @@ public class carrito_Domicilio extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel_CP;
+    private javax.swing.JLabel jLabel_Ciudad;
+    private javax.swing.JLabel jLabel_calle;
+    private javax.swing.JLabel jLabel_estado;
+    private javax.swing.JLabel jLabel_noExt;
+    private javax.swing.JTextField jTextField_CP;
+    private javax.swing.JTextField jTextField_NoExt;
+    private javax.swing.JTextField jTextField_calle;
+    private javax.swing.JTextField jTextField_ciudad;
+    private javax.swing.JTextField jTextField_estado;
     // End of variables declaration//GEN-END:variables
 }
