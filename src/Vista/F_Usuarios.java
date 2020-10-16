adf10c2a4ddde65a6cf6feafd6d85593cc79d747
Eduardo <al047554@uacam.mx>
2020-10-16 12:02:19 -0500

    Diseño de Banner
    Se añade color al banner del usuario.

--- src/Vista/F_Usuarios.java
+++ src/Vista/F_Usuarios.java
@@ -29,21 +29,45 @@ public class F_Usuarios extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jScrollBar1 = new javax.swing.JScrollBar();
+        jPanel_banner = new javax.swing.JPanel();
+        jLabel_productos = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jPanel_banner.setBackground(new java.awt.Color(185, 226, 140));
+
+        jLabel_productos.setFont(new java.awt.Font("Tahoma", 1, 36)); // NOI18N
+        jLabel_productos.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Vista/iconos/mercado.png"))); // NOI18N
+        jLabel_productos.setText("Productos");
+
+        javax.swing.GroupLayout jPanel_bannerLayout = new javax.swing.GroupLayout(jPanel_banner);
+        jPanel_banner.setLayout(jPanel_bannerLayout);
+        jPanel_bannerLayout.setHorizontalGroup(
+            jPanel_bannerLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel_bannerLayout.createSequentialGroup()
+                .addGap(35, 35, 35)
+                .addComponent(jLabel_productos)
+                .addContainerGap(518, Short.MAX_VALUE))
+        );
+        jPanel_bannerLayout.setVerticalGroup(
+            jPanel_bannerLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel_bannerLayout.createSequentialGroup()
+                .addContainerGap(45, Short.MAX_VALUE)
+                .addComponent(jLabel_productos)
+                .addGap(41, 41, 41))
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGap(0, 743, Short.MAX_VALUE)
-                .addComponent(jScrollBar1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+            .addComponent(jPanel_banner, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jScrollBar1, javax.swing.GroupLayout.DEFAULT_SIZE, 404, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jPanel_banner, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 425, Short.MAX_VALUE))
         );
 
         pack();
@@ -85,6 +109,7 @@ public class F_Usuarios extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JScrollBar jScrollBar1;
+    private javax.swing.JLabel jLabel_productos;
+    private javax.swing.JPanel jPanel_banner;
     // End of variables declaration//GEN-END:variables
 }
