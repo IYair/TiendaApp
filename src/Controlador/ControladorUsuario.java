ef90577bdfbf586ccfc9fb1e0488c9a385ef671c
Yair <al046638@uacam.mx>
2020-10-17 17:34:19 -0500

    Formant ControladorUsuario
    Se hace la accion Format para el documento ControladorUsuario.

--- src/Controlador/ControladorUsuario.java
+++ src/Controlador/ControladorUsuario.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Controlador;
 
 import Modelo.CRUD_Usuario;
@@ -16,43 +11,46 @@ import javax.swing.JOptionPane;
  *
  * @author Final
  */
-public class ControladorUsuario implements ActionListener{
-    
+public class ControladorUsuario implements ActionListener {
+
     private Usuario modeloUsuario;
     private CRUD_Usuario modeloCRUDUsuario;
     private Register vistaUsuario;
-    
-    public ControladorUsuario(Usuario usuario, CRUD_Usuario crudusuario, Register formularioUsuario){
-     this.modeloUsuario=usuario;
-     this.vistaUsuario=formularioUsuario;
-     this.modeloCRUDUsuario=crudusuario;
-     this.vistaUsuario.jButtonRegistarse.addActionListener(this);
+
+    public ControladorUsuario(Usuario usuario, CRUD_Usuario crudusuario, Register formularioUsuario) {
+        this.modeloUsuario = usuario;
+        this.vistaUsuario = formularioUsuario;
+        this.modeloCRUDUsuario = crudusuario;
+
+        this.vistaUsuario.jButtonRegistarse.addActionListener(this);
     }
-    public void iniciar(){
+
+    public void iniciar() {
         vistaUsuario.setTitle("Crear Usuario");
         vistaUsuario.setLocationRelativeTo(null);
     }
+
     @Override
-    public void actionPerformed(ActionEvent e){
+    public void actionPerformed(ActionEvent e) {
         modeloUsuario.setPassword(vistaUsuario.jPasswordContraseña.getText());
         modeloUsuario.setNombre(vistaUsuario.jTextNombre.getText());
         modeloUsuario.setApellido(vistaUsuario.jTextApellido.getText());
         modeloUsuario.setCorreo(vistaUsuario.jTextCorreo.getText());
         modeloUsuario.setDateborn(vistaUsuario.jTextDateborn.getText());
-    
+
         if (modeloCRUDUsuario.RegistrarUsuario(modeloUsuario)) {
-            JOptionPane.showMessageDialog(null,"Usuario Creado Correctamente");
-        }
-        else{
-            JOptionPane.showMessageDialog(null,"Error al crear Usuario");
+            JOptionPane.showMessageDialog(null, "Usuario Creado Correctamente");
+        } else {
+            JOptionPane.showMessageDialog(null, "Error al crear Usuario");
         }
     }
-    public void Limpiar(){
-            vistaUsuario.jPasswordContraseña.setText(null);
-            vistaUsuario.jTextNombre.setText(null);
-            vistaUsuario.jTextApellido.setText(null);
-            vistaUsuario.jPasswordContraseña.setText(null);
-            vistaUsuario.jTextDateborn.setText(null);
-            
-        }
+
+    public void Limpiar() {
+        vistaUsuario.jPasswordContraseña.setText(null);
+        vistaUsuario.jTextNombre.setText(null);
+        vistaUsuario.jTextApellido.setText(null);
+        vistaUsuario.jPasswordContraseña.setText(null);
+        vistaUsuario.jTextDateborn.setText(null);
+
+    }
 }
