ecbc99ede3beccdfeafd786c1c318c9056462613
Yair <al046638@uacam.mx>
2020-10-18 19:48:04 -0500

    Importacion de librerias CRUD_Usuario
    Se importan las librerías a utilizar para los diferentes métodos.

--- src/Modelo/CRUD_Usuario.java
+++ src/Modelo/CRUD_Usuario.java
@@ -8,7 +8,12 @@ package Modelo;
 import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 
 /**
  *
