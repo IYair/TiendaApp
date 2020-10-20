2aba44bc79f3bd6f06ac80808b38ba82118df7dc
Yair <al046638@uacam.mx>
2020-10-19 23:02:13 -0500

    Clase Productos
    Variables de productos.

---
+++ src/Modelo/Producto.java
@@ -0,0 +1,54 @@
+package Modelo;
+
+/**
+ *
+ * @author Final
+ */
+public class Producto {
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getDescripcion() {
+        return Descripcion;
+    }
+
+    public void setDescripcion(String Descripcion) {
+        this.Descripcion = Descripcion;
+    }
+
+    public String getCategoria() {
+        return Categoria;
+    }
+
+    public void setCategoria(String Categoria) {
+        this.Categoria = Categoria;
+    }
+
+    public Double getPrecio() {
+        return Precio;
+    }
+
+    public void setPrecio(Double Precio) {
+        this.Precio = Precio;
+    }
+
+    private int id;
+    private String Nombre;
+    private String Descripcion;
+    private String Categoria;
+    private Double Precio;
+}
