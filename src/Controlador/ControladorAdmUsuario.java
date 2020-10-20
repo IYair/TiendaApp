a44ce3016a6e5eaab05c536dfe4ad6c72296a40a
Yair <al046638@uacam.mx>
2020-10-19 23:01:00 -0500

    ControladorAdmUsuario.

--- src/Controlador/ControladorAdmUsuario.java
+++ src/Controlador/ControladorAdmUsuario.java
@@ -1,14 +1,70 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Controlador;
+
+import Modelo.CRUD_Usuario;
+import Modelo.Usuario;
+import Vista.F_Administrador;
+import Vista.Register;
+import Vista.Usuarios;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.SQLException;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author Final
  */
-public class ControladorAdmUsuario {
+public class ControladorAdmUsuario implements ActionListener {
+
+    private Usuario modeloUsuario;
+    private CRUD_Usuario modeloUsuarioCRUD;
+    private Usuarios vistaUsuarios;
+
+    public ControladorAdmUsuario(Usuario usuario, CRUD_Usuario crudUsuario, Usuarios vistaUsuarios) {
+        this.vistaUsuarios = vistaUsuarios;
+        this.modeloUsuario = usuario;
+        this.modeloUsuarioCRUD = crudUsuario;
+        
+        this.vistaUsuarios.jButtonActualizar.addActionListener(this);
+        this.vistaUsuarios.jButtonInsertar.addActionListener(this);
+    }
+    public void iniciar(){
+        vistaUsuarios.setTitle("Administracion de usuarios");
+        vistaUsuarios.setLocationRelativeTo(null);
+    }
+    @Override
+    public void actionPerformed (ActionEvent e){
+        if (e.getSource()== vistaUsuarios.jButtonActualizar) {
+            mostrarTablaUsuarios();
+        }
+        if(e.getSource()==vistaUsuarios.jButtonInsertar){
+            Usuario modelo = new Usuario();
+            CRUD_Usuario crudUsuario = new CRUD_Usuario();
+            Register vista = new Register();
+            ControladorRegister controlador = new ControladorRegister(modelo, crudUsuario, vista);
+            controlador.iniciar();
+            vista.setVisible(true);
+            vista.setLocationRelativeTo(null);
+            vista.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        }
+        if(e.getSource()== vistaUsuarios.btn_home){
+            F_Administrador vistaAdministrador = new F_Administrador();
+            ControladorAdmInicio controlador = new ControladorAdmInicio(vistaAdministrador);
+            controlador.iniciar();
+            vistaAdministrador.setVisible(true);
+            vistaAdministrador.pack();
+            vistaAdministrador.setLocationRelativeTo(null);
+            vistaAdministrador.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
+        }
+        if (e.getSource() == vistaUsuarios.btn_eliminar) {
+            
+        }
+    }
+
+    public void mostrarTablaUsuarios(){
+        CRUD_Usuario p = new CRUD_Usuario();
+            vistaUsuarios.jTableUsuarios.setModel(p.ObtenerUsuario());
+    }
     
 }
