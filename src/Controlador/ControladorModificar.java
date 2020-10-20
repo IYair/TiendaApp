1807197f3f3c3edc5e04fdd76c78cc3f13f84194
Yair <al046638@uacam.mx>
2020-10-20 12:55:00 -0500

    Funcionalidad boton Modificar.

--- src/Controlador/ControladorModificar.java
+++ src/Controlador/ControladorModificar.java
@@ -2,9 +2,11 @@ package Controlador;
 
 import Modelo.CRUD_Usuario;
 import Modelo.Usuario;
+import Vista.Usuarios;
 import Vista.modificar_Usuario;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -14,6 +16,7 @@ public class ControladorModificar implements ActionListener{
     private Usuario modeloUsuario;
     private CRUD_Usuario crudUsuario;
     private modificar_Usuario vistaModificar;
+    public Usuarios vistaUsuarios;
     
     public ControladorModificar(Usuario modeloUsuario, CRUD_Usuario crudUsuario, modificar_Usuario vistaModificar) {
         this.modeloUsuario = modeloUsuario;
@@ -33,10 +36,37 @@ public class ControladorModificar implements ActionListener{
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        String x =String.valueOf(vistaUsuarios.n);
+        vistaModificar.jTextId.setText(x);
         if(e.getSource()== vistaModificar.jButton_cancelar){
         
         }
+        if(e.getSource()== vistaModificar.jButton_modificar){
+            
+            modeloUsuario.setId(Integer.parseInt(vistaModificar.jTextId.getText()));
+            modeloUsuario.setPassword(vistaModificar.jTextField_contraseña.getText());
+            modeloUsuario.setNombre(vistaModificar.jTextField_nombre.getText());
+            modeloUsuario.setApellido(vistaModificar.jTextField_apellido.getText());
+            modeloUsuario.setCorreo(vistaModificar.jTextField_correo.getText());
+            modeloUsuario.setDateborn(vistaModificar.jTextField_fechaNacimiento.getText());
+
+            if (crudUsuario.EditarUsuario(modeloUsuario)) {
+                JOptionPane.showMessageDialog(null, "Usuario Creado Correctamente");
+                Limpiar();
+            } else {
+                JOptionPane.showMessageDialog(null, "Error al crear Usuario");
+                Limpiar();
+            }
+        }
         
+    }
+    public void Limpiar() {
+        vistaModificar.jTextField_contraseña.setText(null);
+        vistaModificar.jTextField_nombre.setText(null);
+        vistaModificar.jTextField_apellido.setText(null);
+        vistaModificar.jTextField_correo.setText(null);
+        vistaModificar.jTextField_fechaNacimiento.setText(null);
+
     }
 
 }
